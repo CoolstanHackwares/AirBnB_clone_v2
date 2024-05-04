0858b9779e3f9ce2ea8e6f04149cecc3b719b39a
CoolstanHackwares <stanley_mbakwe@yahoo.com>
2024-05-03 17:32:54 -0700

    Compress before sending

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,33 +1,32 @@
 #!/usr/bin/python3
-#A Fabfile that generates a .tgz archive from the contents of web_static
-
-from fabric.api import local
+#A Fabfile that generates a .tgz archive from the contents of web_static.
+import os.path
 from datetime import datetime
+from fabric.api import local
+
 
 def do_pack():
-    """
-    Generates a .tgz archive from the contents of the web_static folder
-    """
+    """Create a tar gzipped archive of the directory web_static."""
     try:
-        # Get the current date and time
-        now = datetime.now()
-        timestamp = now.strftime("%Y%m%d%H%M%S")
+        dt = datetime.utcnow()
+        timestamp = dt.strftime("%Y%m%d%H%M%S")
+        archive_name = "versions/web_static_{}.tgz".format(timestamp)
 
         # Create the versions folder if it doesn't exist
         local("mkdir -p versions")
 
-        # Generate the archive filename
-        archive_name = "versions/web_static_{}.tgz".format(timestamp)
-
         # Create the .tgz archive
         local("tar -cvzf {} web_static".format(archive_name))
 
-        print("web_static packed: {} -> {}Bytes".format(
-            archive_name,
-            local("du -b {}".format(archive_name), capture=True)
-        ))
-        return archive_name
+        # Check if the archive file exists
+        if os.path.isfile(archive_name):
+            print("web_static packed: {} -> {}Bytes".format(
+                archive_name,
+                os.path.getsize(archive_name)
+            ))
+            return archive_name
+        else:
+            return None
     except Exception as e:
         print(e)
         return None
-
