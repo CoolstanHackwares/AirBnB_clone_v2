1bcac068008679dddd031532e3c97717b6bbc724
CoolstanHackwares <stanley_mbakwe@yahoo.com>
2024-05-19 22:50:53 +0000

    Python is cool!

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+"""Starts a Flask web application.
+The application listens on 0.0.0.0, port 5000.
+Routes:
+    /: Displays 'Hello HBNB!'.
+    /hbnb: Displays 'HBNB'.
+    /c/<text>: Displays 'C' followed by <text> value.
+    /python/(<text>): Displays 'Python' followed by <text> value.
+"""
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello_hbnb():
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def c(text):
+    # Replace underscores with spaces
+    text = text.replace("_", " ")
+    return "C {}".format(text)
+
+
+@app.route("/python/", defaults={"text": "is cool"}, strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def python(text):
+    # Replace underscores with spaces
+    text = text.replace("_", " ")
+    return "Python {}".format(text)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
