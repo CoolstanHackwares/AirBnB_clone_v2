b35bd8fbbb46ccf2edfd6a58c49cb86b3abb3e1d
CoolstanHackwares <stanley_mbakwe@yahoo.com>
2024-05-20 15:36:48 +0000

    q

--- web_flask/100-hbnb.py
+++ web_flask/100-hbnb.py
@@ -21,10 +21,10 @@ def hbnb():
 
 
 @app.teardown_appcontext
-def teardown(exc):
+def teardown(exception):
     """Remove the current SQLAlchemy session."""
     storage.close()
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0")
+    app.run(host="0.0.0.0", port=5000)
